6cbd976484d6dacbbddcb8450bd4534029956897
Deekshant <deekshantsharma6@gmail.com>
2021-08-03 10:02:20 -0700

    done

--- src/App.js
+++ src/App.js
@@ -1,30 +1,32 @@
+import { Container } from "@material-ui/core";
 import axios from "axios";
 import { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
-        const [word, setWord] = useState("");
-        const [meanings, setMeanings] = useState([]);
+  const [word, setWord] = useState("");
+  const [meanings, setMeanings] = useState([]);
 
   const dictionaryapi = async () => {
     try {
       const data = await axios.get(
         "https://api.dictionaryapi.dev/api/v2/entries/en/plane"
       );
-//       console.log(data.data);
-      setMeanings(data.data)
+      //       console.log(data.data);
+      setMeanings(data.data);
     } catch (error) {
       console.log(error);
     }
   };
 
-
   useEffect(() => {
-          dictionaryapi();
-  }, [])
+    dictionaryapi();
+  }, []);
   return (
-    <div>
-      <h1>Hello world</h1>
+    <div style={{height:'100vh', background:'#282c34',color:"white"}}>
+      <Container max="md" style={{display:"flex",flexDirection:"column",height:"100vh"}}>
+Dictionary
+      </Container>
     </div>
   );
 }
