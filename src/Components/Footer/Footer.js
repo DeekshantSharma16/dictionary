44727727e556358ebe5178f9f53b01c9ce288d36
Deekshant <deekshant@ubitechsolutions.com>
2021-08-05 13:15:43 +0530

    footer changed

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -5,13 +5,30 @@ const Footer = () => {
   return (
     <div className="footer">
       <hr style={{ width: "90%", marginTop: 20 }} />
-
+Get connected with us on social networks:
       <div className="iconContainer">
-        <a href="##" target="__blank">
-          <i className="fab fa-instagram-square fa-2x"></i>
+        <a href="https://www.instagram.com/deekshantsharma_/" target="__blank">
+          <p class="instagram">
+            <i class="fab fa-instagram fa-2x"></i>
+          </p>
         </a>
-        <a href="https://www.linkedin.com/in/deekshant-sharma-752347139" target="__blank">
-          <i className="fab fa-linkedin fa-2x"></i>
+        <a
+          href="https://www.linkedin.com/in/deekshant-sharma-752347139"
+          target="__blank"
+        >
+          <i className="fab fa-linkedin-in fa-2x"></i>
+        </a>
+        <a
+          href="https://github.com/deeksh1008"
+          target="__blank"
+        >
+          <i className="fab fa-github fa-2x"></i>
+        </a>
+        <a
+          href="https://mail.google.com/mail/u/1/#inbox"
+          target="__blank"
+        >
+          <i className="fab fa-google fa-2x"></i>
         </a>
       </div>
     </div>
